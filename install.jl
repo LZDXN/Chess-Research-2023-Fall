d8ba035d767e294c9f9bbd36a3c980da59ae6c6b
LZDXN <47139542+LZDXN@users.noreply.github.com>
2023-11-22 18:53:31 -0500

    edited python package requirement and update julia package version

--- install.jl
+++ install.jl
@@ -1,9 +1,8 @@
-import Pkg
-Pkg.add("PyCall")
-Pkg.add("Pandas")
-Pkg.add("ProgressMeter")
+using Pkg
+Pkg.add(PackageSpec(name="IJulia", version="1.24.2"))
+Pkg.add(PackageSpec(name="ProgressMeter", version="1.9.0"))
+Pkg.add(PackageSpec(name="PyCall", version="1.96.2"))
 
-Pkg.build("PyCall")
-Pkg.build("Pandas")
+Pkg.build("IJulia")
 Pkg.build("ProgressMeter")
-
+Pkg.build("PyCall")
