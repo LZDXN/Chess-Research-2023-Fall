4b1a7a5d07c3b4cae94fab1fe12979ef59c9045f
Ubuntu <ubuntu@ip-172-26-2-120.us-east-2.compute.internal>
2023-12-14 06:32:25 +0000

    finished the new pipeline 1 pre-check with log

--- install.jl
+++ install.jl
@@ -2,7 +2,12 @@ import Pkg
 Pkg.add("PyCall")
 Pkg.add("Pandas")
 Pkg.add("ProgressMeter")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+
 
 Pkg.build("PyCall")
 Pkg.build("Pandas")
 Pkg.build("ProgressMeter")
+Pkg.build("CSV")
+Pkg.build("DataFrames")
