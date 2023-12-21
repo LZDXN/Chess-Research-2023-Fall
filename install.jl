455af4634e46097010ada9acfb1b3ae2f0742988
Ubuntu <ubuntu@ip-172-26-2-120.us-east-2.compute.internal>
2023-12-21 04:12:00 +0000

    add csv

--- install.jl
+++ install.jl
@@ -1,13 +1,10 @@
 import Pkg
 Pkg.add("PyCall")
 Pkg.add("Pandas")
-Pkg.add("ProgressMeter")
 Pkg.add("CSV")
 Pkg.add("DataFrames")
 
-
 Pkg.build("PyCall")
 Pkg.build("Pandas")
-Pkg.build("ProgressMeter")
 Pkg.build("CSV")
 Pkg.build("DataFrames")
