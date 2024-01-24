b9fe0b62bd150db63f4ec5b638a95f1d1d225d7a
Ubuntu <ubuntu@ip-172-26-2-120.us-east-2.compute.internal>
2024-01-24 04:02:50 +0000

    added comparison graph

--- install.jl
+++ install.jl
@@ -5,9 +5,12 @@ Pkg.add(PackageSpec(name="PyCall", version="1.96.2"))
 Pkg.add(PackageSpec(name="CSV", version="0.10.11"))
 Pkg.add(PackageSpec(name="DataFrames", version="1.6.1"))
 # Pkg.add(PackageSpec(name="Chess", version="0.7.5"))
+Pkg.add("Plots")
+
 
 Pkg.build("IJulia")
 Pkg.build("PyCall")
 Pkg.build("CSV")
 Pkg.build("DataFrames")
+Pkg.build("Plots")
 # Pkg.build("Chess")
